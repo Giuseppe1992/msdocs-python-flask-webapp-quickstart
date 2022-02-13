295ced801cac08b533df7a8d114f26a47232644d
Giuseppe Di Lena <giuseppedilena92@gmail.com>
2022-02-13 21:32:16 +0100

    add db query

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pyodbc
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
@@ -20,6 +21,24 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+
+@app.route("/query", methods=["GET"])
+def query():
+   server = 'app-service-db-server-north-europe.database.windows.net'
+   database = 'app-service-db-north-europe'
+   username = 'giuseppe'
+   password = 'G1useppe'   
+   driver= '{ODBC Driver 17 for SQL Server}'
+
+   with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+      with conn.cursor() as cursor:
+         cursor.execute("SELECT * FROM Persons")
+         row = cursor.fetchone()
+         while row:
+               print (str(row[2]))
+               row = cursor.fetchone()
+   name = str(row[2])
+   return render_template('query.html', name = name)
 
 if __name__ == '__main__':
    app.run()
