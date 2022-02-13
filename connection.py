482e616e50f46c7cbba983bb01ee504dd03bdec9
Giuseppe Di Lena <giuseppedilena92@gmail.com>
2022-02-13 19:53:20 +0100

    connection.py ssh testing

---
+++ connection.py
@@ -0,0 +1,14 @@
+import pyodbc
+server = 'app-service-db-server-north-europe.database.windows.net'
+database = 'app-service-db-north-europe'
+username = 'giuseppe'
+password = 'G1useppe'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
